ff526c1d8cd2272a93ed0cdbb0c0833002a452ea
Abhinav Minhas <abhinavminhas@users.noreply.github.com>
2021-10-06 11:49:54 +1100

    Assertion addition to tests

--- GmailAPIHelper.CORE.Tests/GmailTests.cs
+++ GmailAPIHelper.CORE.Tests/GmailTests.cs
@@ -10,8 +10,9 @@ namespace GmailAPIHelper.CORE.Tests
         [TestCategory("GMAIL-TESTS-DOTNETCORE")]
         public void Test1_GetLatestMessage()
         {
-            var message = GmailHelper.GetGmailService(ApplicatioName, GmailHelper.TokenPathType.WORKING_DIRECTORY)
+            var message = GmailHelper.GetGmailService(ApplicatioName)
                 .GetLatestMessage(query: "[from:test.auto.helper@gmail.com][subject:'TEST']in:inbox is:read", markRead: true);
+            Assert.IsNotNull(message);
             TestContext.WriteLine(message);
         }
     }
--- GmailAPIHelper.NET.Tests/GmailTests.cs
+++ GmailAPIHelper.NET.Tests/GmailTests.cs
@@ -10,8 +10,9 @@ namespace GmailAPIHelper.NET.Tests
         [TestCategory("GMAIL-TESTS-DOTNETFRAMEWORK")]
         public void Test1_GetLatestMessage()
         {
-            var message = GmailHelper.GetGmailService(ApplicatioName, GmailHelper.TokenPathType.WORKING_DIRECTORY)
+            var message = GmailHelper.GetGmailService(ApplicatioName)
                 .GetLatestMessage(query: "[from:test.auto.helper@gmail.com][subject:'TEST']in:inbox is:read", markRead: true);
+            Assert.IsNotNull(message);
             TestContext.WriteLine(message);
         }
     }
