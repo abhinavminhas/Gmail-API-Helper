7039b77e556ce0c5bea3f37fe485ed0524c564ef
abhinavminhas <abhinavminhas@users.noreply.github.com>
2024-07-28 16:31:16 +1000

    ModifyMessage/ModifyMessages - Throw 'ArgumentNullException' instead of 'NullReferenceException'

--- GmailAPIHelper.NET.Tests/GmailTests.cs
+++ GmailAPIHelper.NET.Tests/GmailTests.cs
@@ -882,7 +882,7 @@ namespace GmailAPIHelper.NET.Tests
                 Assert.Fail("No Exception Thrown.");
             }
             catch (AssertFailedException ex) { throw ex; }
-            catch (ArgumentNullException ex) { Assert.AreEqual("Value cannot be null. (Parameter '<labelsToAdd> / <labelsToRemove> required.')", ex.Message); }
+            catch (ArgumentNullException ex) { Assert.AreEqual("Value cannot be null.\r\nParameter name: <labelsToAdd> / <labelsToRemove> required.", ex.Message); }
         }
 
         [TestMethod]
@@ -930,7 +930,7 @@ namespace GmailAPIHelper.NET.Tests
                 Assert.Fail("No Exception Thrown.");
             }
             catch (AssertFailedException ex) { throw ex; }
-            catch (ArgumentNullException ex) { Assert.AreEqual("Value cannot be null. (Parameter '<labelsToAdd> / <labelsToRemove> required.')", ex.Message); }
+            catch (ArgumentNullException ex) { Assert.AreEqual("Value cannot be null.\r\nParameter name: <labelsToAdd> / <labelsToRemove> required.", ex.Message); }
         }
 
         [TestMethod]
